c8fe992410944c045c2efecdf8d987182b22cc88
Sylvester Stephenson <sylvester@redcore.co.za>
2022-02-13 23:19:19 +0200

    removed useless files

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-
-app.get("/", (req, res) => {
-    return res.status(200).send("Hello, World!");
-});
-
-app.listen(3000, () => "Express running on port 3000");
